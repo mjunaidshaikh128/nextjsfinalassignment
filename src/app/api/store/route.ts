88d9472a37f58350b0d55c8cc63cf18d8b3656ce
mjunaidshaikh128 <115004219+mjunaidshaikh128@users.noreply.github.com>
2023-09-30 17:06:53 +0500

    commented api logic 2

--- src/app/api/store/route.ts
+++ src/app/api/store/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server"
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export async function GET(request: any) {
+// export async function GET(request: any) {
 
-    const clothingStores = await prisma.clothingStore.findMany();
-    return NextResponse.json(clothingStores)
-}
+//     const clothingStores = await prisma.clothingStore.findMany();
+//     return NextResponse.json(clothingStores)
+// }
